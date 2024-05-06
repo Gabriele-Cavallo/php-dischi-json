15e86e361b94227e887e8f909bb564d2e8314bf2
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-06 13:47:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- /Link Axios -->
+    <link rel="stylesheet" href="./css/style.css">
+    <title>PHP Dischi</title>
+</head>
+<body>
+    <!-- Vue-JS HTML Container -->
+    <div id="app">
+        
+    </div>
+    <!-- /Vue-JS HTML Container -->
+    <!-- Vue-JS Script -->
+    <script src="https://unpkg.com/vue@3"></script>
+    <!-- /Vue-JS Script -->
+    <!-- MyJavascript -->
+    <script type="text/javascript" src="./js/script.js"></script>
+    <!-- /MyJavascript -->
+</body>
+</html>
