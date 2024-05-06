1e09fac2dae3ffe054d974a9cd1c68fba4315c77
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-06 18:00:05 +0200

    feat: add query params @ click

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ createApp({
         return {
             discs: [],
             singleDisc: false,
+            activeDisc: '',
         };
     },
     methods: {
@@ -15,20 +16,26 @@ createApp({
             });
         },
         getSingleDiscsFromApi(index){
-            axios.get('server.php')
-            .then((response) => {
-                if(this.discs.length <= 1){
-                    axios.get('server.php')
-                    .then((response) => {
-                        this.discs = response.data;
-                    });
-                    this.singleDisc = false;
-                } else {
+            if(this.discs.length <= 1){
+                axios.get('server.php')
+                .then((response) => {
+                    this.discs = response.data;
+                });
+                this.singleDisc = false;
+            } else {
+                this.activeDisc = index;
+                const queryParams = {
+                    discChoice : this.activeDisc,
+                };
+                axios.get('server.php', {
+                    params: queryParams
+                })
+                .then((response) => {
                     this.discs = [];
-                    this.discs.push(response.data[index]);
-                    this.singleDisc = true;
-                }
-            });
+                    this.discs.push(response.data);
+                });
+                this.singleDisc = true;
+            }
         },
     },
     mounted() {
--- server.php
+++ server.php
@@ -2,6 +2,13 @@
 $discs_json = file_get_contents('./dischi.json');
 $discs = json_decode($discs_json, true);
 
+$response = $discs;
+
+if(isset($_GET['discChoice'])) {
+    $discIndex  = intval($_GET['discChoice']);
+    $response = $discs[$discIndex];
+}
+
 header ('Content-Type: application/json');
-echo json_encode($discs);
+echo json_encode($response);
 ?>
